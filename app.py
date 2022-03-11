37c0c4adbd22d72a3714fc209868b338d8c00bfc
Alex <68344206+avilla2@users.noreply.github.com>
2022-03-10 22:39:34 -0800

    production updates

--- app.py
+++ app.py
@@ -1,1 +1,130 @@
+from joblib import load
+from tensorflow import keras
+import sklearn
+from flask import Flask
+from flask import request
+import os
+import library
+from view import fpage
 
+the_transformer = library.nba_transformer
+file_store = 'models/'
+xgb_model = load(file_store+'xgb_model.joblib')
+logreg_model = load(file_store+'logreg_model.joblib')
+knn_model = load(file_store+'knn_model.joblib')
+ann_model = keras.models.load_model(file_store+'ann_model')
+
+test_df = pd.read_csv(f'test_df.csv')
+logreg_thresholds = pd.read_csv(f'logreg_thresholds.csv').round(2)
+knn_thresholds = pd.read_csv(f'knn_thresholds.csv').round(2)
+xgb_thresholds = pd.read_csv(f'xgb_thresholds.csv').round(2)
+ann_thresholds = pd.read_csv(f'ann_thresholds.csv').round(2)
+
+def handle_data(columns, test_df, the_transformer):
+
+    #massage data coming from user - this is code you need to change to fit your new dataset
+    wins = np.nan
+    if columns['wins'].isdigit() and int(columns['wins'])>0:
+        wins =  int(columns['wins'])  #looks ok
+
+    losses = np.nan
+    if columns['losses'].isdigit() and int(columns['losses'])>=0:
+        losses =  int(columns['losses'])
+
+    ortg = np.nan 
+    if columns['ortg'].isdigit() and int(columns['ortg'])>0:
+        ortg =  int(columns['ortg'])
+    drtg = np.nan
+    if columns['drtg'].isdigit() and int(columns['drtg'])>0:
+        drtg =  int(columns['drtg'])
+    pace = np.nan 
+    if columns['pace'].isdigit() and int(columns['pace'])>0:
+        pace =  int(columns['pace'])
+    srs = np.nan 
+    if columns['srs'].isdigit() and int(columns['srs'])>0:
+        srs =  int(columns['srs'])
+    finish = np.nan if columns['finish']=='unknown' else int(columns['finish'])
+    league = np.nan if columns['league']=='unknown' else columns['league']
+
+    row = dict(W=wins, L=losses, ORtg=ortg, DRtg=drtg, Pace=pace, SRS=srs, Finish=finish, League=league)  #33.0, 'Female', 'C2', 'Southampton', 0.0, 26.0
+
+    #end massaging - you should be able to use the code below as is
+
+    #now add on your new row so can run pipeline
+    n = len(test_df)
+    test_extended = test_df.copy()  #don't mess up original
+    test_extended.loc[n] = np.nan  #add blank row
+
+    #fill in values we have from user
+    for k,v in row.items():
+        test_extended.loc[n, k] = v
+
+    #run pipeline
+    test_transformed = the_transformer.fit_transform(test_extended)
+
+    #grab added row
+    new_row = test_transformed.to_numpy()[-1:]
+
+    #get predictions
+    yhat_xgb, yhat_knn, yhat_logreg, yhat_ann = get_prediction(new_row)  #predict on last row that tacked on.
+    return yhat_xgb, yhat_knn, yhat_logreg, yhat_ann
+
+def get_prediction(row):
+    assert row.shape[0]==1, f'Expecting numpy array but got {row}'
+    #XGBoost
+    xgb_raw = xgb_model.predict_proba(row)  #predict last row, we just tacked on
+    yhat_xgb = xgb_raw[:,1]
+
+    #KNN
+    knn_raw = knn_model.predict_proba(row)
+    yhat_knn = knn_raw[:,1]
+
+    #logreg
+    logreg_raw = logreg_model.predict_proba(row)
+    yhat_logreg = logreg_raw[:,1]
+
+
+    #ANN
+    yhat_ann = ann_model.predict(row)[:,0]
+
+    return [yhat_xgb, yhat_knn, yhat_logreg, yhat_ann]
+
+xgb_table = xgb_thresholds.to_html(index=False, justify='center', col_space=80).replace('<td>', '<td align="center">')
+logreg_table = logreg_thresholds.to_html(index=False, justify='center', col_space=80).replace('<td>', '<td align="center">')
+knn_table = knn_thresholds.to_html(index=False, justify='center', col_space=80).replace('<td>', '<td align="center">')
+ann_table = ann_thresholds.to_html(index=False, justify='center', col_space=80).replace('<td>', '<td align="center">')
+
+fpage = fpage.replace('%xgb_table%', xgb_table)
+fpage = fpage.replace('%logreg_table%', logreg_table)
+fpage = fpage.replace('%knn_table%', knn_table)
+fpage = fpage.replace('%ann_table%', ann_table)
+
+def create_page(page, **fillers):
+  new_page = page[:]  #copy
+  for k,v in fillers.items():
+    new_page = new_page.replace(f'%{str(k)}%', str(v))
+  return new_page
+
+os.environ["FLASK_ENV"] = "development"
+app = Flask(__name__)
+port = 5000
+
+# Define Flask routes
+@app.route("/")
+#This function called when user first enters url into browser
+def home():
+    return create_page(fpage)
+
+@app.route('/data', methods = ['POST'])
+#This function called when user hits Evaluate button
+def data():
+  form_data = request.form
+  print(form_data.to_dict())
+  yhat_xgb, yhat_knn, yhat_logreg, yhat_ann = handle_data(form_data.to_dict(),test_df, the_transformer)  #calling my own function here
+  ensemble = (yhat_xgb[0]+yhat_knn[0]+yhat_logreg[0]+yhat_ann[0])/4.0
+  xgb = np.round(yhat_xgb[0], 2)
+  knn = np.round(yhat_knn[0], 2)
+  logreg = np.round(yhat_logreg[0], 2)
+  ann = np.round(yhat_ann[0], 2)
+  ensemble = np.round(ensemble, 2)
+  return create_page(fpage, xgb=xgb, knn=knn, logreg=logreg, ann=ann, ensemble=ensemble)  #return specs for new page with answer filled in
---
+++ view.py
@@ -0,0 +1,147 @@
+fpage = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+  body {
+    background-color: #E5E4E2;
+  }
+
+
+  h1 {
+    color: maroon;
+    margin-left: 40px;
+  }
+</style>
+</head>
+
+<body>
+
+  <h1>Predict The Chance of Making the NBA playoffs</h1>
+  <img src='https://img.bleacherreport.net/img/images/photos/003/804/554/hi-res-9a9716c6bb102c42af62340eeb4b0bea_crop_north.jpg?1556082334&w=3072&h=2048' height=200>
+
+
+  <form id="row_info" action="data" method = "POST">
+
+        <!-- I am not using the hidden field - just have it in case can think of some need for it later -->
+        <input type='hidden' id='hidden1' value='hidden value'/>
+
+        <p style="font-size:24pt;">Enter Wins <input style="font-size:18pt;" type = "text" name = "wins" placeholder="unknown"/></p>
+        <p style="font-size:24pt;">Enter Loses <input style="font-size:18pt;" type = "text" name = "losses" placeholder="unknown"/></p>
+        <p style="font-size:14pt;">In a Regular NBA season, Wins and Losses add up to 82</p>
+        <p>
+        <label for="finish" style="font-size:24pt;">Choose position in division:</label>
+        <select id="finish" name="finish" style="font-size:18pt;">
+          <option value="unknown">Unknown</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+
+        <p>
+        <p style="font-size:24pt;">Enter Offensive Rating <input style="font-size:18pt;" type = "text" name = "ortg" placeholder="unknown"/></p>
+        <p style="font-size:14pt;">An Estimate of points scored per 100 possesions. Typically between 79 and 118</p>
+        <p style="font-size:24pt;">Enter Defensive Rating <input style="font-size:18pt;" type = "text" name = "drtg" placeholder="unknown"/></p>
+        <p style="font-size:14pt;">An Estimate of points allowed per 100 possesions. Typically between 79 and 118</p>
+        <p>
+
+        <p>
+        <p style="font-size:24pt;">Enter SRS (Strength of Schedule) <input style="font-size:18pt;" type = "text" name = "srs" placeholder="unknown"/></p>
+        <p style="font-size:14pt;">Simple Rating System: a team rating that takes into account average point differential and strength of schedule. 
+          The rating is denominated in points above/below average, where zero is average. Typically between -15 and 12</p>
+        <p style="font-size:24pt;">Enter Pace <input style="font-size:18pt;" type = "text" name = "pace" placeholder="unknown"/></p>
+        <p style="font-size:14pt;">An estimate of possessions per 48 minutes. Typically between 82 and 136</p>
+        <p>
+
+        <label for="league" style="font-size:24pt;">Choose league:</label>
+        <select id="league" name="league" style="font-size:18pt;">
+          <option value="unknown">Unknown</option>
+          <option value="NBA">NBA</option>
+          <option value="ABA">ABA</option>
+          <option value="BAA">BAA</option>
+        </select>
+
+        <p><input style="font-size:24pt;" type = "submit" value = "Evaluate" /></p>
+    </form>
+
+    <script>
+        <!-- toggle_image a bit of misnomer. It can be used to toggle tables on and off as well -->
+        function toggle_image(im_id) {
+            var state = document.getElementById(im_id).style.display;
+            var new_state = 'inline';
+            if (state=='inline'){new_state='none'};
+            document.getElementById(im_id).style.display = new_state;
+        }
+    </script>
+
+    <h1 onmouseover="toggle_image('image1')"
+        onmouseout="toggle_image('image1')"
+        style="display:inline" >  <!-- image1 is pipeline screenshot -->
+      Results - probability of making the playoffs
+    </h1>
+    <ul>
+      <!-- only showing threshold table for each method - might want to add tuning screenshot as well -->
+      <li><h2 onmouseover="toggle_image('xgb')"
+              onmouseout="toggle_image('xgb')"
+              style="display:inline" >
+            XGBoost alone: %xgb%    <!-- filled in once you have a prediction -->
+          </h2></li>
+      <p>
+      <li><h2 onmouseover="toggle_image('knn')"
+              onmouseout="toggle_image('knn')"
+              style="display:inline" >
+            KNN alone: %knn%
+          </h2></li>
+      <p>
+      <li><h2 onmouseover="toggle_image('logreg')"
+              onmouseout="toggle_image('logreg')"
+              style="display:inline" >
+            LogisticRegression alone: %logreg%
+          </h2></li>
+      <p>
+      <li><h2 onmouseover="toggle_image('ann')"
+              onmouseout="toggle_image('ann')"
+              style="display:inline" >
+            ANN alone: %ann%
+          </h2></li>
+      <p>
+      <li><h2>Ensemble: %ensemble%</h2>
+    </ul>
+
+    <!-- Here are onmouseover images and tables - see above -->
+    <div style="position:absolute; top:10px; left:500px">
+      <!-- See notes above for hoops you have to jump through to get link to Drive image -->
+      <img id="image1" style="display:none" src='https://drive.google.com/uc?export=view&id=1l16COqYsx25l5Lxb-JXiOH3JNdbyVcUX' height='400'>
+
+      <!-- Below filled in with html table code when server starts up. They come from csv threshold files from your github. -->
+      <div id="xgb" style='display:none'>
+        %xgb_table%
+        <hr>
+        %xgb_lime_table%
+      </div>
+      <div id="knn" style='display:none'>
+        %knn_table%
+        <hr>
+        %knn_lime_table%
+      </div>
+      <div id="logreg" style='display:none'>
+        %logreg_table%
+        <hr>
+        %logreg_lime_table%
+      </div>
+      <div id="ann" style='display:none'>
+        %ann_table%
+        <hr>
+        %ann_lime_table%
+      </div>
+    </div>
+
+
+
+  </body>
+  </html>
+'''
